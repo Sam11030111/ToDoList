4d30a397cee0fcd3f567cab74a39c3816cd5a48e
李竑陞 <sam86110306@gmail.com>
2023-06-08 11:19:25 +0800

    First Commit(2)

---
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewViewModel.swift
+//  ToDoList
+//
+//  Created by 李竑陞 on 2023/6/7.
+//
+
+import Foundation
+
+class LoginViewViewModel: ObservableObject {
+    
+    @Published var email = ""
+    @Published var password = ""
+    
+    init() {
+        
+    }
+    
+    func login() {
+        
+    }
+    
+    func validate() {
+        
+    }
+}
